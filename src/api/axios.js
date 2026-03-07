8ec8c5735007517517291a6e47f971efcd24ffe3
Mubashirijaz786 <mubashirejaz786@gmail.com>
2026-03-07 10:37:03 +0500

    as

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = 'https://canva-server-production.up.railway.app/api' ;
+ const BASE_URL = 'https://canva-server-production.up.railway.app/api' ;
 // const BASE_URL = 'http://localhost:5000/api';
 
 axios.defaults.withCredentials = true;
